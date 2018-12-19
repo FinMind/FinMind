c3f74ec3900d397647a300a0496ddbf00dd43a3f
Lin Sam <sam.lin@tripsaas.com>
2018-12-19 16:19:32 +0800

    init

--- FinancialMining/API/Load.py
+++ FinancialMining/API/Load.py
@@ -1,7 +1,6 @@
 
 import os, sys
 import importlib
-# __file__ = '/home/linsam/project/master/bitbucket/financialmining/API/Stock.py'
 PATH = "/".join( os.path.abspath(__file__).split('/')[:-2])
 sys.path.append(PATH)
 
@@ -13,7 +12,7 @@ def Load(table = '',select = [],date = ''):
                      'EnergyFuturesPrices']:
         raise(AttributeError, "Hidden attribute")  
     else:
-        data = getattr(importlib.import_module("API.{}".format(table)), table)(
+        data = getattr(importlib.import_module("FinancialMining.API.{}".format(table)), table)(
                 select = select,date = date)
         return data
 #-------------------------------------------------------------------------------------------
@@ -24,84 +23,8 @@ def LoadTableList(table = '',select = [],date = ''):
                      'GovernmentBonds','CrudeOilPrices','EnergyFuturesPrices']:
         raise(AttributeError, "Hidden attribute")  
     else:
-        data = getattr(importlib.import_module("API.{}".format(table)), 'Load_Data_List')()
+        data = getattr(importlib.import_module("FinancialMining.API.{}".format(table)), 'Load_Data_List')()
         return data
-
-
-#---------------------------------------------------------------
-def StockInfo(select = [],date = ''):
-    data = getattr(importlib.import_module("API.{}".format('StockInfo')), 'StockInfo')(
-            select = select,date = date)
-    return data
-
-def StockPrice(select = [],date = ''):
-    data = getattr(importlib.import_module("API.{}".format('StockPrice')), 'StockPrice')(
-            select = select,date = date)
-    return data
-
-def FinancialStatements(select = [],date = ''):
-    data = getattr(importlib.import_module("API.{}".format('FinancialStatements')), 'FinancialStatements')(
-            select = select,date = date)
-    return data
-
-def StockDividend(select = [],date = ''):
-    data = getattr(importlib.import_module("API.{}".format('StockDividend')), 'StockDividend')(
-            select = select,date = date)
-    return data
-
-def ExchangeRate(select = [],date = ''):
-    data = getattr(importlib.import_module("API.{}".format('ExchangeRate')), 'ExchangeRate')(
-            select = select,date = date)
-    return data
-
-def InstitutionalInvestors(select = [],date = ''):
-    data = getattr(importlib.import_module("API.{}".format('InstitutionalInvestors')), 'InstitutionalInvestors')(
-            select = select,date = date)
-    return data
-
-def InterestRate(select = [],date = ''):
-    data = getattr(importlib.import_module("API.{}".format('InterestRate')), 'InterestRate')(
-            select = select,date = date)
-    return data
-
-def GovernmentBonds(select = [],date = ''):
-    data = getattr(importlib.import_module("API.{}".format('GovernmentBonds')), 'GovernmentBonds')(
-            select = select,date = date)
-    return data
-
-#---------------------------------------------------------------
-def StockPriceList():
-    data = getattr(importlib.import_module("API.{}".format('StockPrice')), 'Load_Data_List')()
-    return data
-    
-def FinancialStatementsList():
-    data = getattr(importlib.import_module("API.{}".format('FinancialStatements')), 'Load_Data_List')()
-    return data
-
-
-def StockDividendList():
-    data = getattr(importlib.import_module("API.{}".format('StockDividend')), 'Load_Data_List')()
-    return data
-
-def ExchangeRateList():
-    data = getattr(importlib.import_module("API.{}".format('ExchangeRate')), 'Load_Data_List')()
-    return data
-
-def InterestRateList():
-    data = getattr(importlib.import_module("API.{}".format('InterestRate')), 'Load_Data_List')()
-    return data
-
-def GovernmentBondsList():
-    data = getattr(importlib.import_module("API.{}".format('GovernmentBonds')), 'Load_Data_List')()
-    return data
-
-
-
-
-
-
-
-
 
 
 
