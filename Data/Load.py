586af61534f592aac61cf87aabc7c7162d655c0b
Lin Sam <sam.lin@tripsaas.com>
2019-01-03 00:08:15 +0800

    change func name

--- Data/Load.py
+++ Data/Load.py
@@ -5,25 +5,25 @@ PATH = "/".join( os.path.abspath(__file__).split('/')[:-2])
 sys.path.append(PATH)
 
 #---------------------------------------------------------------
-def Load(table = '',select = [],date = ''):
+def FinData(table = '',select = [],date = ''):
     if table not in ['StockInfo','StockPrice','FinancialStatements',
                      'StockDividend','ExchangeRate','InstitutionalInvestors',
                      'InterestRate','GovernmentBonds','CrudeOilPrices',
                      'EnergyFuturesPrices']:
         raise(AttributeError, "Hidden attribute")  
     else:
-        data = getattr(importlib.import_module("FinMind.API.{}".format(table)), table)(
+        data = getattr(importlib.import_module("FinMind.Data.{}".format(table)), table)(
                 select = select,date = date)
         return data
 #-------------------------------------------------------------------------------------------
 
-def LoadTableList(table = '',select = [],date = ''):
+def FinDataList(table = '',select = [],date = ''):
     if table not in ['StockPrice','FinancialStatements',
                      'StockDividend','ExchangeRate','InterestRate',
                      'GovernmentBonds','CrudeOilPrices','EnergyFuturesPrices']:
         raise(AttributeError, "Hidden attribute")  
     else:
-        data = getattr(importlib.import_module("FinMind.API.{}".format(table)), 'Load_Data_List')()
+        data = getattr(importlib.import_module("FinMind.Data.{}".format(table)), 'Load_Data_List')()
         return data
 
 
--- demo.py
+++ demo.py
@@ -1,67 +1,67 @@
 
 
-from FinMind.API import Load
+from FinMind.Data import Load
 #---------------------------------------------------------------
 
-data = Load.Load(table = 'StockInfo',select = [],date = '')
+data = Load.FinData(table = 'StockInfo',select = [],date = '')
 #---------------------------------------------------------------
-data = Load.LoadTableList(table = 'StockPrice')
+data = Load.FinDataList(table = 'StockPrice')
 print( data[:5] )
-data = Load.Load(table = 'StockPrice',
+data = Load.FinData(table = 'StockPrice',
                  select = ['2330'],date = '2018-10-10')
 print( data[:5] )
 
 #---------------------------------------------------------------
-data = Load.LoadTableList(table = 'FinancialStatements')
+data = Load.FinDataList(table = 'FinancialStatements')
 print( data[:5] )
-data = Load.Load(table = 'FinancialStatements',
+data = Load.FinData(table = 'FinancialStatements',
                  select = ['2330'],date = '2017-10-10')
 print( data[:5] )
 
 #---------------------------------------------------------------
-data = Load.LoadTableList(table = 'StockDividend')
+data = Load.FinDataList(table = 'StockDividend')
 print( data[:5] )
-data = Load.Load(table = 'StockDividend',
+data = Load.FinData(table = 'StockDividend',
                  select = ['2330'],date = '2017-10-10')
 print( data[:5] )
 
 #---------------------------------------------------------------
-data = Load.LoadTableList(table = 'ExchangeRate')
+data = Load.FinDataList(table = 'ExchangeRate')
 print( data[:5] )
-data = Load.Load(table = 'ExchangeRate',
+data = Load.FinData(table = 'ExchangeRate',
                  select = ['JPY'],date = '2018-10-10')
 print( data[:5] )
 
 #---------------------------------------------------------------
-data = Load.Load(table = 'InstitutionalInvestors',
+data = Load.FinDataList(table = 'InstitutionalInvestors',
                  date = '2018-10-10')
 print( data[:5] )
 
 #---------------------------------------------------------------
-data = Load.LoadTableList(table = 'InterestRate')
+data = Load.FinDataList(table = 'InterestRate')
 print( data[:5] )
-data = Load.Load(table = 'InterestRate',
+data = Load.FinData(table = 'InterestRate',
                  select = ['BCB'],date = '2018-10-10')
 print( data[:5] )
 
 #---------------------------------------------------------------
-data = Load.LoadTableList(table = 'GovernmentBonds')
+data = Load.FinDataList(table = 'GovernmentBonds')
 print( data[:5] )
-data = Load.Load(table = 'GovernmentBonds',
+data = Load.FinData(table = 'GovernmentBonds',
                  select = ['Canada 1-Month'],date = '2018-10-10')
 print( data[:5] )
 
 #---------------------------------------------------------------
-data = Load.LoadTableList(table = 'CrudeOilPrices')
+data = Load.FinDataList(table = 'CrudeOilPrices')
 print( data[:5] )
-data = Load.Load(table = 'CrudeOilPrices',
+data = Load.FinData(table = 'CrudeOilPrices',
                  select = ['Brent'],date = '2018-10-10')
 print( data[:5] )
 
 #---------------------------------------------------------------
-data = Load.LoadTableList(table = 'EnergyFuturesPrices')
+data = Load.FinDataList(table = 'EnergyFuturesPrices')
 print( data[:5] )
-data = Load.Load(table = 'EnergyFuturesPrices',
+data = Load.FinData(table = 'EnergyFuturesPrices',
                  select = ['Brent Oil Futures'],date = '2018-10-10')
 print( data[:5] )
 
