e76def0a62a93589f6e73768fe0ae02cb56ad9bb
Lin Sam <sam.lin@tripsaas.com>
2019-01-04 23:40:11 +0800

    change demo.py date to today

--- demo.py
+++ demo.py
@@ -1,35 +1,38 @@
 
 
 from FinMind.Data import Load
-#---------------------------------------------------------------
+import datetime
 
-data = Load.FinData(dataset = 'StockInfo',select = [],date = '')
+date = str( datetime.datetime.now().date() )
+
+#---------------------------------------------------------------
+data = Load.FinData(dataset = 'StockInfo')
 #---------------------------------------------------------------
 data = Load.FinDataList(dataset = 'StockPrice')
 print( data[:5] )
 data = Load.FinData(dataset = 'StockPrice',
-                 select = '2330',date = '2018-10-10')
+                 select = '2330',date = date)
 print( data[:5] )
 
 #---------------------------------------------------------------
 data = Load.FinDataList(dataset = 'FinancialStatements')
 print( data[:5] )
 data = Load.FinData(dataset = 'FinancialStatements',
-                 select = '2330',date = '2017-10-10')
+                 select = '2330',date = date)
 print( data[:5] )
 
 #---------------------------------------------------------------
 data = Load.FinDataList(dataset = 'StockDividend')
 print( data[:5] )
 data = Load.FinData(dataset = 'StockDividend',
-                 select = '2330',date = '2017-10-10')
+                 select = '2330',date = date)
 print( data[:5] )
 
 #---------------------------------------------------------------
 data = Load.FinDataList(dataset = 'ExchangeRate')
 print( data[:5] )
 data = Load.FinData(dataset = 'ExchangeRate',
-                 select = 'JPY',date = '2018-10-10')
+                 select = 'JPY',date = date)
 print( data[:5] )
 
 #---------------------------------------------------------------
@@ -37,39 +40,39 @@ data = Load.FinDataList(dataset = 'InstitutionalInvestors')
 print( data[:5] )
 
 data = Load.FinData(dataset = 'InstitutionalInvestors',
-                 select = 'Dealer',date = '2018-10-10')
+                 select = 'Dealer',date = date)
 print( data[:5] )
 
 #---------------------------------------------------------------
 data = Load.FinDataList(dataset = 'InterestRate')
 print( data[:5] )
 data = Load.FinData(dataset = 'InterestRate',
-                 select = 'BCB',date = '2018-10-10')
+                 select = 'BCB',date = date)
 print( data[:5] )
 
 #---------------------------------------------------------------
 data = Load.FinDataList(dataset = 'GovernmentBonds')
 print( data[:5] )
 data = Load.FinData(dataset = 'GovernmentBonds',
-                 select = 'Canada 1-Month',date = '2018-10-10')
+                 select = 'Canada 1-Month',date = date)
 print( data[:5] )
 
 #---------------------------------------------------------------
 data = Load.FinDataList(dataset = 'CrudeOilPrices')
 print( data[:5] )
 data = Load.FinData(dataset = 'CrudeOilPrices',
-                 select = 'Brent',date = '2018-10-10')
+                 select = 'Brent',date = date)
 print( data[:5] )
 
 #---------------------------------------------------------------
 data = Load.FinDataList(dataset = 'EnergyFuturesPrices')
 print( data[:5] )
 data = Load.FinData(dataset = 'EnergyFuturesPrices',
-                 select = 'Brent Oil Futures',date = '2018-10-10')
+                 select = 'Brent Oil Futures',date = date)
 print( data[:5] )
 #---------------------------------------------------------------
 data = Load.FinData(dataset = 'GoldPrice',
-                 date = '2018-10-10')
+                 date = date)
 print( data[:5] )
 
 
