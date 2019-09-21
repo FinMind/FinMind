8047d56f5ba4418015ce72711924076f544aac4d
lin sam <samlin266118@gmail.com>
2019-09-22 00:57:49 +0800

    Update api_demo.py

--- example/api_demo.py
+++ example/api_demo.py
@@ -32,6 +32,17 @@ temp = res.json()
 data = pd.DataFrame(temp['data'])
 data.head()
 
+'''----------------TaiwanStockPriceMinute----------------'''
+form_data = {'dataset':'TaiwanStockPriceMinute',
+             'stock_id':'2330',
+             'date':'2019-06-01'}
+res = requests.post(
+        url,verify = True,
+        data = form_data)
+
+temp = res.json()
+data = pd.DataFrame(temp['data'])
+data.head()
 
 '''----------------FinancialStatements----------------'''
 form_data = {'dataset':'FinancialStatements',
@@ -46,6 +57,17 @@ data = pd.DataFrame(temp['data'])
 data = Load.transpose(data)
 data.head()
 
+'''----------------TaiwanCashFlowsStatement----------------'''
+form_data = {'dataset':'TaiwanCashFlowsStatement',
+             'stock_id':'2330',
+             'date':'2019-06-01'}
+res = requests.post(
+        url,verify = True,
+        data = form_data)
+
+temp = res.json()
+data = pd.DataFrame(temp['data'])
+data.head()
 
 '''----------------TaiwanStockStockDividend----------------'''
 form_data = {'dataset':'TaiwanStockStockDividend',
@@ -190,6 +212,18 @@ data.head()
 '''----------------USStockPrice----------------'''
 form_data = {'dataset':'USStockPrice',
              'stock_id':'^DJI',
+             'date':'2019-06-01'}
+res = requests.post(
+        url,verify = True,
+        data = form_data)
+
+temp = res.json()
+data = pd.DataFrame(temp['data'])
+data.head()
+
+'''----------------USStockPriceMinute----------------'''
+form_data = {'dataset':'USStockPriceMinute',
+             'stock_id':'MTX',
              'date':'2019-06-01'}
 res = requests.post(
         url,verify = True,
@@ -398,6 +432,18 @@ temp = res.json()
 data = pd.DataFrame(temp['data'])
 data.head()
 
+'''----------------RawMaterialFuturesPricesMinute----------------'''
+form_data = {'dataset':'RawMaterialFuturesPricesMinute',
+             'data_id':'London Robusta Coffee Futures',
+             'date':'2019-08-01'}
+res = requests.post(
+        url,verify = True,
+        data = form_data)
+
+temp = res.json()
+data = pd.DataFrame(temp['data'])
+data.head()
+
 '''----------------GoldPrice----------------'''
 form_data = {'dataset':'GoldPrice','date':'2019-06-06'}
 res = requests.post(
@@ -450,38 +496,3 @@ temp = res.json()
 data = pd.DataFrame(temp['data'])
 data.head()
 
-'''----------------TaiwanCashFlowsStatement----------------'''
-form_data = {'dataset':'TaiwanCashFlowsStatement',
-             'stock_id':'2330',
-             'date':'2019-06-01'}
-res = requests.post(
-        url,verify = True,
-        data = form_data)
-
-temp = res.json()
-data = pd.DataFrame(temp['data'])
-data.head()
-
-'''----------------TaiwanStockPriceMinute----------------'''
-form_data = {'dataset':'TaiwanStockPriceMinute',
-             'stock_id':'2330',
-             'date':'2019-06-01'}
-res = requests.post(
-        url,verify = True,
-        data = form_data)
-
-temp = res.json()
-data = pd.DataFrame(temp['data'])
-data.head()
-
-'''----------------RawMaterialFuturesPricesMinute----------------'''
-form_data = {'dataset':'RawMaterialFuturesPricesMinute',
-             'data_id':'London Robusta Coffee Futures',
-             'date':'2019-08-01'}
-res = requests.post(
-        url,verify = True,
-        data = form_data)
-
-temp = res.json()
-data = pd.DataFrame(temp['data'])
-data.head()
