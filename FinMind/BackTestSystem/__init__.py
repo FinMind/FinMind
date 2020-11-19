093401324586724ec8d31fc271386d222204301e
YenChen <yenchen0416@gmail.com>
2020-11-19 22:41:39 +0800

    add:schema to module path

--- FinMind/BackTestSystem/__init__.py
+++ FinMind/BackTestSystem/__init__.py
@@ -1,3 +1,4 @@
 from FinMind.BackTestSystem.BaseClass import BackTest, Strategy
 from FinMind.BackTestSystem import Strategies
 from FinMind.BackTestSystem import BaseClass, utils
+from FinMind.BackTestSystem import Schema
