3307bc5284c7e326fdf863bf3bfee5e6ae5abee3
ReasonTsai <76902669+ReasonTsai@users.noreply.github.com>
2021-04-09 21:24:56 +0800

    Add files via upload

---
+++ FinMind/BackTestSystem/Strategies/VolumnCross.py
@@ -0,0 +1,34 @@
+class Volumn(Strategy):
+    """
+    summary:
+        5日均量與10日均量黃金交叉且上漲 買入
+        5日均量與10日均量死亡交叉且下跌 賣出
+    """
+    kdays1 = 5
+    kdays2 = 10
+
+    def create_trade_sign(self, stock_price: pd.DataFrame) -> pd.DataFrame:
+        stock_price = stock_price.sort_values("date")
+        volumn10 = 0
+        volumn5 = 0
+        stock_price["ma5"] = np.round(stock_price["Trading_Volume"].rolling(window = 5, center = False).mean(), 2)
+        stock_price["ma10"] = np.round(stock_price["Trading_Volume"].rolling(window = 10, center = False).mean(), 2)
+        stock_price['ma5-10'] = stock_price['ma5'] - stock_price['ma10']
+        stock_price['diff'] = np.sign(stock_price['ma5-10'])
+        stock_price.loc[(stock_price.index < self.kdays2), "diff"] = np.nan
+        stock_price['signal'] = 0
+        stock_price.loc[
+            (
+                np.sign(stock_price['diff'] - stock_price['diff'].shift(1)) > 0
+                & (stock_price['spread'] > 0)
+            ),
+            "signal",
+        ] = 1
+        stock_price.loc[
+            (
+                np.sign(stock_price['diff'] - stock_price['diff'].shift(1)) < 0
+                & (stock_price['spread'] < 0)
+            ),
+            "signal",
+        ] = -1
+        return stock_price
