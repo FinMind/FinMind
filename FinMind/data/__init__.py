7eca3d2de686589086397bbde8dfa259df17f890
Featasy <featasy@gmail.com>
2021-05-22 19:23:29 +0800

    Add DataSubscriber

--- FinMind/data/__init__.py
+++ FinMind/data/__init__.py
@@ -1,2 +1,5 @@
 from FinMind.data.finmind_api import FinMindApi
 from FinMind.data.data_loader import DataLoader
+from FinMind.data.data_subscriber import DataSubscriber
+from FinMind.data.data_subscriber import Stock
+from FinMind.data.data_subscriber import FutureAndOption
