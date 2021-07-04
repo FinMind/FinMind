a2407012ca36ed0be6d355cfa72de6a63cd10fc6
sam <samlin266118@gmail.com>
2021-07-04 21:31:38 +0800

    feat: add test line schema

---
+++ tests/schema/test_line.py
@@ -0,0 +1,34 @@
+import os
+
+import pytest
+
+from FinMind.data import DataLoader
+from FinMind.schema import LinePlotSchema
+
+
+@pytest.fixture(scope="module")
+def df():
+    user_id = os.environ.get("FINMIND_USER", "")
+    password = os.environ.get("FINMIND_PASSWORD", "")
+    data_loader = DataLoader()
+    data_loader.login(user_id, password)
+    df = data_loader.taiwan_stock_month_revenue(
+        stock_id="2890", start_date="2018-1M", end_date="2021-7M"
+    )
+    df["labels"] = (
+        df[["revenue_year", "revenue_month"]]
+        .astype(str)
+        .agg(lambda date: f"{date[0]}-{date[1]}M", axis=1)
+    )
+    df["series"] = df["revenue"].agg(lambda value: round(value / 1e8, 2))
+    return df
+
+
+def test_LinePlotSchema(df):
+    line_plot_data = LinePlotSchema(**df.to_dict("list"))
+    assert isinstance(line_plot_data, LinePlotSchema)
+
+
+def test_LinePlotSchema_df_convert(df):
+    line_plot_data = LinePlotSchema.df_convert(df)
+    assert isinstance(line_plot_data, LinePlotSchema)
