90dcb12cfa9949ef30a9060900e02105728580ea
sam <samlin266118@gmail.com>
2021-07-07 22:45:17 +0800

    feat: df["revenue"].agg to map

--- tests/schema/test_plot.py
+++ tests/schema/test_plot.py
@@ -20,7 +20,7 @@ def df():
         .astype(str)
         .agg(lambda date: f"{date[0]}-{date[1]}M", axis=1)
     )
-    df["series"] = df["revenue"].agg(lambda value: round(value / 1e8, 2))
+    df["series"] = df["revenue"].map(lambda value: round(value / 1e8, 2))
     return df
 
 
