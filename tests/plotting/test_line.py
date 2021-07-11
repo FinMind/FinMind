be9b2163e816e7e10a0f3c8081d5ea66350bcc3d
sam <samlin266118@gmail.com>
2021-07-11 21:21:32 +0800

    feat: apply to map

--- tests/plotting/test_line.py
+++ tests/plotting/test_line.py
@@ -24,7 +24,7 @@ def test_line(stock_id, start_date, end_date):
         .astype(str)
         .apply(lambda date: f"{date[0]}-{date[1]}M", axis=1)
     )
-    df["series"] = df["revenue"].apply(lambda value: round(value * 1e-8, 2))
+    df["series"] = df["revenue"].map(lambda value: round(value * 1e-8, 2))
     assert plotting.line(labels=df["labels"], series=df["series"])
 
 
